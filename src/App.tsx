9ff2ac179bd448e9f374311339d9e05f9cf3531a
morgankar <realrohankar03@gmail.com>
2025-06-07 21:16:44 +0530

    Portfolio raw file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
- import './App.css'
+import './App.css'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Projects from './Projects'
 
 function App() {
 
 
   return (
     <div className="Container border-6 border-black w-[100vw] h-[100vh]">
-      
+      <Navbar/>
+      <Hero/>
+      <Projects/>
     </div>
   )
 }
