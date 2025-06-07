c66299a1b03982333dadd975a2db00b6fc385ca6
morgankar <realrohankar03@gmail.com>
2025-06-07 22:17:26 +0530

    wip1

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
 function Hero() {
   return (
-    <div className='container border-2'>
+    <div className='container border-2 m-auto mt-40 '>
         <div className='bg-amber-400 rounded-[50%] w-[148px] h-[148px]'></div>
-        <h1 className='inter-font font-[600] '>Hey,I'm Morgan A. Kar,Developer & Designer</h1>
+        <h1 className='font-[700] text-4xl border-2 max-w-[400px]'>Hey, I'm Morgan A. Kar, Developer & Designer</h1>
     </div>
   )
 }
