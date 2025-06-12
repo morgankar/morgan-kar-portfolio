34f3c6cafd87c4a1ce5510e49726245b2a18b1b8
morgankar <realrohankar03@gmail.com>
2025-06-12 23:57:01 +0530

    Navbar basic layout sketch

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='m-auto text-center'>Navbar</div>
+    <div className='m-auto text-center border-2 flex justify-center items-center'>
+      <div className='card w-[50px] h-[50px] border'></div>
+      <div className='card w-[50px] h-[50px] border'></div>
+      <div className='card w-[50px] h-[50px] border'></div>
+      <div className='card w-[50px] h-[50px] border'></div>
+    </div>
   )
 }
 
