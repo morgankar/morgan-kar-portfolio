a775d6a5feded165415dbe494f4edda86837a988
morgankar <realrohankar03@gmail.com>
2025-06-13 20:37:03 +0530

    navbar wip

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='m-auto text-center border-2 flex justify-center items-center'>
-      <div className='card w-[50px] h-[50px] border'></div>
-      <div className='card w-[50px] h-[50px] border'></div>
-      <div className='card w-[50px] h-[50px] border'></div>
-      <div className='card w-[50px] h-[50px] border'></div>
+    <div className='m-auto text-center  flex justify-center items-center mt-10 mb-30 gap-2'>
+      <div className='card w-[50px] h-[50px] border rounded-md'></div>
+      <div className='card w-[50px] h-[50px] border rounded-md'></div>
+      <div className='card w-[50px] h-[50px] border rounded-md'></div>
+      <div className='card w-[50px] h-[50px] border rounded-md'></div>
     </div>
   )
 }
