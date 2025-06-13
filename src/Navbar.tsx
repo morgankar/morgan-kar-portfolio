a09d9fb42668b5dc9568b5dc1fe817ef9b88fca1
morgankar <realrohankar03@gmail.com>
2025-06-13 21:26:53 +0530

    Added All Icons in navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,10 +3,22 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='m-auto text-center  flex justify-center items-center mt-10 mb-30 gap-2'>
-      <div className='card w-[50px] h-[50px] border rounded-md'></div>
-      <div className='card w-[50px] h-[50px] border rounded-md'></div>
-      <div className='card w-[50px] h-[50px] border rounded-md'></div>
-      <div className='card w-[50px] h-[50px] border rounded-md'></div>
+      <div className='card w-[50px] h-[50px] border rounded-md'>
+        <span><img className='m-auto mt-2' src="icon_x.svg" alt="x icon" /></span>
+      </div>
+      <div className='card w-[50px] h-[50px] border rounded-md'>
+         <span><img className='m-auto mt-1.5' src="icon_github.svg" alt="x icon" /></span>
+      </div>
+      <div className='card w-[50px] h-[50px] border rounded-md'>
+         <span><img className='m-auto mt-2' src="icon_mail.svg" alt="x icon" /></span>
+      </div>
+      <div className='card w-[50px] h-[50px] border rounded-md'>
+         <span><img className='m-auto mt-2' src="icon_linkedin.svg" alt="x icon" /></span>
+      </div>
+       <div className='card w-[50px] h-[50px] border rounded-md'>
+         <span><img className='m-auto mt-2' src="page.svg" alt="x icon" /></span>
+      </div>
+      
     </div>
   )
 }
